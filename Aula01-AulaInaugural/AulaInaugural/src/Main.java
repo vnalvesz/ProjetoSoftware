6fda55ae2d38423cd22037715bc7ea016cd40a35
aluno cmc <alunocmc@umc.br>
2026-08-03 10:54:02 -0300

    Aula Inaugural

---
+++ Aula01-AulaInaugural/AulaInaugural/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int num = 200;
+        double num2 = 200.60;
+        num = 300;
+    }
+}
